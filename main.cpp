7994c3ee0eade6ff88d75c21a8c1a117de555941
jussi.kampela <jussi.kampela@gmail.com>
2025-12-14 10:47:43 +0200

    Made code more humane

--- main.cpp
+++ main.cpp
@@ -16,13 +16,21 @@ bool mole(std::string huh, std::string line) {
 }
 
 std::vector<int> combinations(std::string line) {
+    std::vector<int> all;
+    
+    std::sort(line.begin(), line.end());
+    if (!(std::unique(line.begin(), line.end()) == line.end())) {
+        return all;
+    }
 
     int length = static_cast<int>(line.length());
-    std::vector<int> all;
     auto rng = std::default_random_engine{};
+    int howMany = 0;
+    std::string huh;
 
-    if (length == 4) {
-        int howMany = 24;
+    switch (length) {
+    case 4:
+        howMany = 24;
         while (all.size() < howMany) {
             std::shuffle(line.begin(), line.end(), rng);
             int rivi = std::stoi(line);
@@ -30,10 +38,9 @@ std::vector<int> combinations(std::string line) {
                 all.push_back(rivi);
             }
         }
-    }
-    else if (length == 3) {
-        int howMany = 36;
-        std::string huh;
+        break;
+    case 3:
+        howMany = 36;
         while (all.size() < howMany) {
             for (char c : line) {
                 huh = line + c;
@@ -43,12 +50,12 @@ std::vector<int> combinations(std::string line) {
                     all.push_back(rivi);
                 }
             }
-        } 
-    }
-    else if (length == 2) {
-        int howMany = 14;
+        }
+        break;
+    case 2:
+        howMany = 14;
         for (int i = 0; i < howMany; i++) {
-            std::string huh;
+            huh.clear();
             for (int j = 0; j < 4; j++) {
                 if ((i >> j) & 1) {
                     huh += line[0];
@@ -61,12 +68,10 @@ std::vector<int> combinations(std::string line) {
                 all.push_back(stoi(huh));
             }
         }
+        break;
     }
     return all;
 }
-
-
-
 
 int main() {
     while (true) {
@@ -75,7 +80,15 @@ int main() {
         std::getline(std::cin, line);
         std::cout << std::endl;
 
+        if (line.length() > 4 or line.length() < 2) {
+            std::cout << "Must input betweeen 2 to 4 numbers" << std::endl << std::endl;
+            continue;
+        }
+
         std::vector<int> combs = combinations(line);
+        if (combs.empty()) {
+            std::cout << "All numbers must be different" << std::endl;
+        }
 
         std::sort(combs.begin(), combs.end(), compare);
 
